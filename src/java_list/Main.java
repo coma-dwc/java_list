5f4756e6320270bb629777aa613fd9e59ef9e167
coma-dwc <tamagomapon@gmail.com>
2023-08-19 15:43:26 +0900

    Listメソッド 要素数の取得 finish

--- src/java_list/Main.java
+++ src/java_list/Main.java
@@ -1,12 +1,48 @@
 package java_list;
 
+//LinkedListの作成
+//import java.util.LinkedList;
 //ArrayListの作成
 import java.util.ArrayList;
 import java.util.List;
 
+//public class Main {
+//	public static void main(String[] args) {
+//		List<String> list = new ArrayList<>();
+//		System.out.println(list);
+//	}
+
 public class Main {
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>();
-		System.out.println(list);
+//		List<String> list = new LinkedList<>();
+		//整数を扱うListを作成
+		//Listに要素の追加
+//		List<Integer> list = new LinkedList<>();
+		List<Integer> list = new ArrayList<>();
+//		list.add(1);
+//		list.add(2);
+		
+		//要素の指定位置への追加
+//		list.add(2);
+//		list.add(4);
+//		
+//		list.add(0, 1);
+//		list.add(2, 3);
+		
+		//要素の参照
+//		list.add(1);
+//		list.add(2);
+//		list.add(3);
+		
+		//要素数の取得
+		list.add(1);
+		list.add(2);
+		
+//		System.out.println(list);
+		
+		//Listの最初の要素と3番目の要素を参照
+//		System.out.println(list.get(0));
+//		System.out.println(list.get(2));
+		System.out.println(list.size());
 	}
 }
