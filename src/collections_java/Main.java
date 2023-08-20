b6c40bb3f7d7e7b92079f5cc0f00e7190c2db857
coma-dwc <tamagomapon@gmail.com>
2023-08-20 14:11:36 +0900

    Collections.unmodifiableListの使用

--- src/collections_java/Main.java
+++ src/collections_java/Main.java
@@ -8,8 +8,16 @@ public class Main {
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<>();
 		//Collections.addAllを使用して複数の要素をListに追加
-		Collections.addAll(list, "aaa", "bbb", "ccc");
+//		Collections.addAll(list, "aaa", "bbb", "ccc");
+//		
+//		System.out.println(list);
 		
-		System.out.println(list);
+		
+		//Collections.unmodifiableListを使用して変更不可のListを取得
+		list.add("aaa");
+		list.add("bbb");
+		
+		List<String> list2 = Collections.unmodifiableList(list);
+		list2.add("ccc");  //この行がエラーになる
 	}
 }
